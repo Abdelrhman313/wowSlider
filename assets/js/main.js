e17ecbcb4c4e3e3d2da9a9754cf52f0e48b25b82
Abdelrhman_313 <abdelrhmanyasser77@gmail.com>
2021-02-21 23:09:14 +0200

    'newEdit'

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,25 @@ $(() => {
         e,
         preventDefault();
     }
+
+    // Scroll Body
+    $(window).on('load', function(){
+        $('html, body').animate({scrollTop:0}, 100);
+    });
+    //scroll to bottom
+    setInterval(function(){
+
+        //time to scroll to bottom
+        $("html, body").animate({ scrollTop: $(document).height() }, 60000);
+
+        //scroll to top
+        setTimeout(function() {
+        $('html, body').animate({scrollTop:0}, 60000);
+        },500);//call every 2000 miliseconds
+
+    },2000);//call every 2000 miliseconds
+
+
     // Navigation
     links = [...$('nav ul li a')];
     links.forEach(link => {
